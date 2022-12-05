bb60b6ce2d7e2bf8f7654960150b77d70cc1ddd9
RonnyKn <ronnykurniawn@gmail.com>
2022-12-05 12:06:40 +0700

    adding responsiveness navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,20 @@
 import "./Navbar.css"
-import React from "react"
+import React, { useState } from "react"
 import Logo from "../../data/images/logo.png"
 import { Link, NavLink } from "react-router-dom"
 import { links } from "../../data/data"
 import { GoThreeBars } from "react-icons/go"
+import { MdOutlineClose } from "react-icons/md"
 
 const Navbar = () => {
+  const [isNavShowing, setIsNavShowing] = useState(false)
   return (
     <nav>
       <div className="container nav-container">
         <Link to="/" className="nav-logo">
           <img src={Logo} alt="Logo" />
         </Link>
-        <ul className="nav-links">
+        <ul className={`nav-links ${isNavShowing ? "show-nav" : "hide-nav"}`}>
           {links.map((link, index) => (
             <li key={index}>
               <NavLink
@@ -24,8 +26,11 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <button className="nav-togle-btn">
-          <GoThreeBars />
+        <button
+          className="nav-toggle-btn"
+          onClick={() => setIsNavShowing(!isNavShowing)}
+        >
+          {isNavShowing ? <MdOutlineClose /> : <GoThreeBars />}
         </button>
       </div>
     </nav>
